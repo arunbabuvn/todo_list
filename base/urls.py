8a6666d437a88027155bb54fa5e37d82f814c0b6
arunbabuvn <arunbabuvn@gmail.com>
2023-10-12 10:35:41 +0530

    added Create and Delete functionality

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_page, name="login"),
     path("register/", views.register, name="register"),
-    path("newTask", views.new_task, name="newTask"),
+    path("newTask/", views.new_task, name="newTask"),
+    path("editTask/", views.edit_task, name="editTask"),
+    path("delete/<str:pk>/", views.delete, name="delete"),
+    path("edit/<str:pk>", views.edit, name="edit"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import TaskData
 
 
 # Create your views here.
 def home(request):
-    return render(request, "todo.html")
+    data = TaskData.objects.all()
+    return render(request, "todo.html", {"data": data})
+
+
+def delete(request, pk):
+    data = TaskData.objects.get(id=pk)
+    data.delete()
+    return redirect("/")
+
+
+def edit(request, pk):
+    data = TaskData.objects.get(id=pk)
+    return redirect("/")
 
 
 def login_page(request):
@@ -19,7 +32,7 @@ def login_page(request):
             return render(request, "registration/login.html", context)
 
         login(request, user)
-        return redirect("/")
+        return redirect("home")
 
     return render(request, "registration/login.html")
 
@@ -29,4 +42,14 @@ def register(request):
 
 
 def new_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        description = request.POST.get("description")
+        data = TaskData(title=title, description=description)
+        data.save()
+        return redirect("/")
     return render(request, "newTask.html")
+
+
+def edit_task(request):
+    return render(request, "editTask.html")
